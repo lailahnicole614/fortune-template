1018c466948d8271a5a7cd9bb6fc59aee8a1abdb
Lailah Berry <lailahnicole614@gmail.com>
2023-01-06 15:49:05 -0800

    added colors and text wight in css

--- app.js
+++ app.js
@@ -11,13 +11,50 @@ const resetBtn = document.getElementById('reset');
 
 /* Events */
 submitBtn.addEventListener('click', () => {
-// hide prompt
-// display image
-toggleSections ();
-// pick a random number betwen 0 and length of the answers array - 1
-const randomInt = Math.floor(Math.random()* answers. length);
-// console.log(randomChoice); 
-constrandomInt=Math.floor(Math.random)(x)answers.length 
+    // hide prompt
+    // display image
+    toggleSections();
+    // pick a random number betwen 0 and length of the answers array - 1
+    const randomInt = Math.floor(Math.random() * answers.length);
+    // console.log(randomInt);
+    const randomChoice = answers[randomInt];
+    //console.log(randomChoice);
+    //console.log('answers[12]'), answers[12]);
+    //set the content of the answer p to the random choice
+    answerP.textContent = randomChoice;
+});
+
+resetBtn.addEventListener('click', () => {
+    // hide 8 ball and the answer
+    //show prompt
+});
+
+function toggleSections() {
+    promptSection.classList.toggle('hide');
+    fortuneSection.classList.toggle('hide');
+}
+
+const answers = [
+    'You deserve love',
+    'You are worthy of respect',
+    'Everything you need is inside of you',
+    'Yes, you can',
+    'Treat yourself',
+    'Nothing is too hard for you',
+    'All things are working for your good',
+    'You have plently to offer',
+    'You bring joy to those around you',
+    'You are an important part of your loved ones life',
+    'You are beautiful',
+    'Your shape or size does not dictate your worth',
+    'Money comes to you easily',
+    'You have money in your account',
+    'You have food on your table',
+    'Everyday brings something to be grateful for',
+    'You have all you need to succeed',
+    'You belong here',
+    'You have nothing to prove',
+];
 /* Display Functions */
 
 // (don't forget to call any display functions you want to run on page load!)
