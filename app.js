b7ee4ed2cc20e54c66994cfeaecf00ee8f21c85c
Lailah Berry <lailahnicole614@gmail.com>
2023-01-06 14:36:08 -0800

    Added header in HTML

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 /* Imports */
 
 /* Get DOM Elements */
+const affirmmeBtn = document.getElementById('Affirm Me');
+const promptSection = document.getElementById('prompt');
+const affirmationSection = document.getElementById('Affirmation');
+const answerP = document.getElementById('answer');
+const resetBtn = document.getElementById('reset');
 
 /* State */
 
