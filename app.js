d30ef9d3841d586c09d25aa0045eac7161551352
Lailah Berry <lailahnicole614@gmail.com>
2023-01-06 15:18:34 -0800

    Added toggle sections

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 /* Imports */
 
 /* Get DOM Elements */
-const affirmmeBtn = document.getElementById('Affirm Me');
+const affirmmeBtn = document.getElementById('affirm me');
 const promptSection = document.getElementById('prompt');
-const affirmationSection = document.getElementById('Affirmation');
+const affirmationSection = document.getElementById('affirmation');
 const answerP = document.getElementById('answer');
 const resetBtn = document.getElementById('reset');
 
 /* State */
 
 /* Events */
-submitBtn. addEventListener('click') () =>
+submitBtn.addEventListener('click', () => {
 // hide prompt
 // display image
-functiontoggleSections () {promptSection.classList.toggle('hide')}
-const answers=
+toggleSections ();
+// pick a random number betwen 0 and length of the answers array - 1
+const randomInt = Math.floor(Math.random()* answers. length);
+// console.log(randomChoice); 
 constrandomInt=Math.floor(Math.random)(x)answers.length 
 /* Display Functions */
 
